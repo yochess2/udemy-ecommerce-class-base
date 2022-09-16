c348a30f46e3b6d9380f66542b2c9a2156bc7df9
unknown <drakewang@gmail.com>
2022-09-16 15:07:14 -0700

    Setup navbar

--- src/App.js
+++ src/App.js
@@ -4,8 +4,24 @@ class NavBar extends Component {
 	render() {
 		return (
 			<>
-				<h1>Hello</h1>
-				<p>paragraph</p>
+				<nav className="navbar navbar-expand-lg bg-dark navbar-dark navbar-style">
+				  <div className="container-fluid">
+
+				    <a className="navbar-brand" href="/#">My App</a>
+				    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				      <span className="navbar-toggler-icon"></span>
+				    </button>
+
+				    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+				      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+				        <li className="nav-item">
+				          <a className="nav-link" href="/#">Home</a>
+				        </li>
+				      </ul>
+				    </div>
+
+				  </div>
+				</nav>
 			</>
 		)
 	}
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import "jquery";
 import "popper.js/dist/umd/popper";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
+
+import "./index.css"
 import NavBar from "./App";
 
 // ReactDOM.render(<NavBar/>, document.getElementById("root"));
