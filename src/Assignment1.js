680ee4e4d5163bba7732fea5f87db6d694cead43
unknown <drakewang@gmail.com>
2022-09-16 22:33:57 -0700

    Find solution, no css yet, not refactored

--- src/Assignment1.js
+++ src/Assignment1.js
@@ -1,6 +1,19 @@
 import React, { Component } from "react";
 
 class Assignment1 extends Component {
+	//Question 1: JS doesn't have private methods right? 
+	//  So I assume best practice is to denote private methods with an underscore.
+	//Question 2: How do hoisting work in classes? 
+	//  I think I would need to declare and define this method before it is called.
+	//  I would receive an error if this method is declared before being invoked on line 26.
+
+	//input: int a, int b
+	//returns the product of num1 and num2
+	_multiply = (a,b) => a*b;
+
+
+	//Question 3: How do I grab the price of price and quantity dynamically within each object's netPrice?
+	//  I had to hardcode the netPrice for each object.
 	state = {
 		pageTitle: "Assignment 1",
 		productsCount: 3,
@@ -10,20 +23,36 @@ class Assignment1 extends Component {
 			productName: "abc",
 			price: 10,
 			quantity: 1,
+			netPrice: this._multiply(10,1), //how do i do this._multiply(price,quantity) efficiently?
 		},
 		{
 			id: 2,
 			productName: "abc",
 			price: 20,
 			quantity: 2,
+			netPrice: this._multiply(20,2),
 		},
 		{
 			id: 3,
 			productName: "abc",
 			price: 30,
 			quantity: 3,
+			netPrice: this._multiply(30,3),
 		},
 	]}
+
+
+	//input: int price, string key, int id
+	//side effect: updates the product key's value, and then re-renders
+	onValueChange = (price, key, id) => {
+		let products = this.state.products;
+
+		let product = this.state.products.find(product => product.id === id);
+		product[key] = price;
+		product.netPrice = this._multiply(product.price, product.quantity);
+
+		this.setState([...products])
+	}
 
 	render() {
 		return <>
@@ -47,12 +76,22 @@ class Assignment1 extends Component {
 								<td>{product.id}</td>
 								<td>{product.name}</td>
 								<td>
-									{product.price}
+									<input 	type="text" 
+											defaultValue={product.price} 
+											onChange={
+												e => { this.onValueChange(e.target.value, 'price', product.id) }
+											}
+									/>
 								</td>
 								<td>
-									{product.quantity}
+									<input 	type="text" 
+											defaultValue={product.quantity} 
+											onChange={
+												e => { this.onValueChange(e.target.value, 'quantity', product.id) }
+											}
+									/>
 								</td>
-								<td>{product.price * product.quantity}</td>
+								<td>{product.netPrice}</td>
 							</tr>
 						)
 					})
