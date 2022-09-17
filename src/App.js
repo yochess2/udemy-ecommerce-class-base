86a5a24c991fad6f64e3d3e0c1d6117573a719e7
unknown <drakewang@gmail.com>
2022-09-16 21:19:25 -0700

    Setup assignment1 with math part done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import NavBar from "./NavBar";
 import MainContent from "./MainContent";
 
+import Assignment1 from "./Assignment1"
+
 class App extends Component {
 	render() {
 		return <>
 			<NavBar />
-			<MainContent />
+			<Assignment1 />
 		</>
 	}
 }
--- src/Assignment1.js
+++ src/Assignment1.js
@@ -1,8 +1,80 @@
 import React, { Component } from "react";
 
 class Assignment1 extends Component {
+	state = {
+		pageTitle: "Assignment 1",
+		productsCount: 3,
+		products: [
+		{
+			id: 1,
+			productName: "abc",
+			price: 10,
+			quantity: 1,
+		},
+		{
+			id: 2,
+			productName: "abc",
+			price: 20,
+			quantity: 2,
+		},
+		{
+			id: 3,
+			productName: "abc",
+			price: 30,
+			quantity: 3,
+		},
+	]}
+
 	render() {
-		return {
-			<h1>Assignment1</h1>
+		return <>
+			<h4>Buy Products</h4>
+			<hr/>
+			<table>
+				<thead>
+					<tr>
+						<td style={ {fontWeight: "bold"} }>#</td>
+						<td style={ {fontWeight: "bold"} }>Product Name</td>
+						<td style={ {fontWeight: "bold"} }>Price</td>
+						<td style={ {fontWeight: "bold"} }>Quantity</td>
+						<td style={ {fontWeight: "bold"} }>Net Price</td>
+					</tr>
+				</thead>
+				<tbody>
+				{
+					this.state.products.map((product) => {
+						return (
+							<tr key={product.id}>
+								<td>{product.id}</td>
+								<td>{product.name}</td>
+								<td>
+									{product.price}
+								</td>
+								<td>
+									{product.quantity}
+								</td>
+								<td>{product.price * product.quantity}</td>
+							</tr>
+						)
+					})
+				}
+				<tr></tr>
+				<tr>
+					<td></td>
+					<td></td>
+					<td></td>
+					<td>Total</td>
+					<td>
+						{
+							this.state.products
+								.map(product => product.price * product.quantity)
+								.reduce((memo,num) => memo+num)
+						}
+					</td>
+				</tr>
+				</tbody>
+			</table>
+		</>
 	}
 }
+
+export default Assignment1;
