c76016f7eaab8c6e67334a01ebdfd00dbec5ef0f
unknown <drakewang@gmail.com>
2022-09-18 04:56:08 -0700

    Uploading resources from course to figure out bug issue

---
+++ src/ShoppingCart.jsx
@@ -0,0 +1,117 @@
+import React, { Component } from "react";
+import Product from "./Product";
+
+export default class ShoppingCart extends Component {
+  //Executes when the component is mounted
+  constructor(props) {
+    console.log("constructor - ShoppingCart");
+    super(props); //calling super class's constructor
+
+    //initialization of the state
+    this.state = {
+      products: [
+        { id: 1, productName: "iPhone", price: 8900, quantity: 0 },
+        { id: 2, productName: "Sony Camera", price: 4500, quantity: 0 },
+        { id: 3, productName: "Samsung QLED TV", price: 7745, quantity: 0 },
+        { id: 4, productName: "iPad Pro", price: 12400, quantity: 0 },
+        { id: 5, productName: "Xbox", price: 7780, quantity: 0 },
+        { id: 6, productName: "Dell Monitor", price: 880, quantity: 0 },
+      ],
+    };
+  }
+
+  render() {
+    console.log("render - ShoppingCart");
+
+    return (
+      <div className="container-fluid">
+        <h4>Shopping Cart</h4>
+
+        <div className="row">
+          {this.state.products.map((prod) => {
+            return (
+              <Product
+                key={prod.id}
+                product={prod}
+                onIncrement={this.handleIncrement}
+                onDecrement={this.handleDecrement}
+                onDelete={this.handleDelete}
+              >
+                <button className="btn btn-primary">Buy Now</button>
+              </Product>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+  // render ends here
+
+  //Executes after constructor and render method (includes life cycle of child components, if any) of current component
+  componentDidMount() {
+    //fetch data from data source
+    console.log("componentDidMount - ShoppingCart");
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log(
+      "componentDidUpdate",
+      prevProps,
+      prevState,
+      this.props,
+      this.state
+    );
+
+    if (prevProps.x != this.props.x) {
+      //make http call
+    }
+  }
+
+  //executes when the user clicks on + button.
+  handleIncrement = (product, maxValue) => {
+    //get index of selected product
+    let allProducts = [...this.state.products];
+    let index = allProducts.indexOf(product);
+
+    allProducts = allProducts.map((p) => {
+      return { ...p };
+    });
+
+    if (allProducts[index].quantity < maxValue) {
+      allProducts[index].quantity = 5;
+
+
+      //update the state of current component (parent component)
+      this.setState({ products: allProducts });
+    }
+  };
+
+  //executes when the user clicks on - button.
+  handleDecrement = (product, minValue) => {
+    //get index of selected product
+    let allProducts = [...this.state.products];
+    let index = allProducts.indexOf(product);
+
+    if (allProducts[index].quantity > minValue) {
+      allProducts[index].quantity--;
+
+      //update the state of current component (parent component)
+      this.setState({ products: allProducts });
+    }
+  };
+
+  //executes when the user clicks on Delete (X) button in the Product component.
+  handleDelete = (product) => {
+    //get index of selected product
+    let allProducts = [...this.state.products];
+    let index = allProducts.indexOf(product);
+
+    if (window.confirm("Are you sure to delete?")) {
+      //delete product based on index
+      allProducts.splice(index, 1);
+
+      //update the state of current component (parent component)
+      this.setState({ products: allProducts });
+    }
+  };
+}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import ReactDOM from "react-dom/client"; 
 import "jquery";
 import "popper.js/dist/umd/popper";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
+import App from "./App";
+import "./index.css";
 import "font-awesome/css/font-awesome.css";
 
-import "./index.css"
-
-import App from "./App";
-
-ReactDOM.render(<App/>, document.getElementById("root"));
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-// 	<React.StrictMode>
-// 		<App/>
-// 	</React.StrictMode>
-// )
+ReactDOM.render(<App />, document.getElementById("root"));
